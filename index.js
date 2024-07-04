b7b53efaa55820a45d2cf20301a464662a8fe1cf
Kashif Mohd <kashifmohd901@gmail.com>
2024-07-03 19:40:42 -0500

    changes in index

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("dist"));
+
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
 
 // Custom token for morgan to log request body
 morgan.token("req-body", (req) => {
